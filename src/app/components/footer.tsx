681f96af9eff8ae7f15495ccfa4b1e6d25840750
David Ajibola <ajiboladavid0963@gmail.com>
2023-09-29 10:28:25 +0100

    made chanhes

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,51 @@
-export default function Footer () {
-    return(
-        <div>
-            
-        </div>
+import leadcity from '../../../public/leadcity.png'
+import Image from 'next/image'
+
+export default function Footer() {
+    return (
+        <footer className=" bg-[#DAE5EB] text-black py-10">
+            <div className="flex justify-center">
+                <ul className="text-center mr-20 space-y-4 ">
+                    <li>Security & Brand</li>
+                    <li>Report copyright Infringement</li>
+                    <li>Report security issue</li>
+                    <li>Trademark Notice</li>
+                </ul>
+                <ul className="text-center mr-20 space-y-4">
+                    <li>Website</li>
+                    <li>Accessibility</li>
+                    <li>Digital Accessibility</li>
+                    <li>Privacy statement</li>
+                </ul>
+                <ul className="text-center space-y-4">
+                    <li>Get in Touch</li>
+                    <li>Contact Leadcity</li>
+                    <li>Maps & Directions</li>
+                    <li>Jobs</li>
+                </ul>
+            </div>
+            <div className='flex flex-col items-center justify-center pt-12'>
+                <div className='text-center'>
+                    <h1 className='text-4xl'>Leadcity</h1>
+                    <p className='pb-2 text-lg'>University</p>
+                </div>
+                <div>
+                    <Image src={leadcity} alt='leadcity' width={70} height={70} />
+                </div>
+            </div>
+            <div className='flex justify-between items-center px-20 pt-4'>
+                <div>
+                    <p>© 2023 The President and Fellows of Leadcity university</p>
+                </div>
+                <div>
+                    <ul className='flex space-x-6'>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>Youtube</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     )
 }
--- src/app/components/youtubeVideo.tsx
+++ src/app/components/youtubeVideo.tsx
@@ -1,7 +1,20 @@
-export default function YoutubeVideo({ videoId }: any) {
+import leadcityOne from '../../../public/leadcity1.png'
+import leadcityTwo from '../../../public/leadcity2.png'
+import leadcityThree from '../../../public/leadcity3.png'
+import leadcityFour from '../../../public/leadcity4.png'
+import leadcityFive from '../../../public/leadcity5.png'
+import leadcitySux from '../../../public/leadcity6.png'
+import Image from 'next/image'
+
+export default function YoutubeVideo() {
+    const images = [leadcityOne, leadcityTwo, leadcityThree, leadcityFour, leadcityFive, leadcitySux];
     return (
-        <div className="flex items-center px-[30%] pb-6 bg-[#F8F8F8]">
-            
-        </div>
+        <div className="flex items-center pb-6 bg-[#F8F8F8] pt-12">
+        {images.map((image, index) => (
+          <div key={index} className="mr-4">
+            <Image src={image} alt={`LeadCity Image ${index + 1}`} width={600} height={600} />
+          </div>
+        ))}
+      </div>
     )
 }
