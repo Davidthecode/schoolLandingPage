412ec31795fcbc75892e492add35b8f4665e1f1d
David Ajibola <ajiboladavid0963@gmail.com>
2023-09-29 12:22:43 +0100

    made changes

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,21 +4,21 @@ import Image from 'next/image'
 export default function Footer() {
     return (
         <footer className="bg-[#DAE5EB] text-black py-10">
-            <div className="flex justify-center">
-                <ul className="text-center mr-20 space-y-4 ">
-                    <li>Security & Brand</li>
+            <div className="flex justify-center smallScreen:flex-col">
+                <ul className="text-center mr-20 smallScreen:mr-0 space-y-4 ">
+                    <li className='font-semibold'>Security & Brand</li>
                     <li>Report copyright Infringement</li>
                     <li>Report security issue</li>
                     <li>Trademark Notice</li>
                 </ul>
-                <ul className="text-center mr-20 space-y-4">
-                    <li>Website</li>
+                <ul className="text-center mr-20 smallScreen:mr-0 space-y-4 smallScreen:mt-4">
+                    <li className='font-semibold'>Website</li>
                     <li>Accessibility</li>
                     <li>Digital Accessibility</li>
                     <li>Privacy statement</li>
                 </ul>
-                <ul className="text-center space-y-4">
-                    <li>Get in Touch</li>
+                <ul className="text-center space-y-4 smallScreen:mt-4">
+                    <li className='font-semibold'>Get in Touch</li>
                     <li>Contact Leadcity</li>
                     <li>Maps & Directions</li>
                     <li>Jobs</li>
@@ -33,7 +33,7 @@ export default function Footer() {
                     <Image src={leadcity} alt='leadcity' width={70} height={70} />
                 </div>
             </div>
-            <div className='flex justify-between items-center px-20 pt-4 extrasmallTablet:flex-col extrasmallTablet:space-y-4'>
+            <div className='flex justify-between items-center px-20 pt-4 extrasmallTablet:flex-col extrasmallTablet:space-y-4 smallScreen:flex-col'>
                 <div>
                     <p>© 2023 The President and Fellows of Leadcity university</p>
                 </div>
